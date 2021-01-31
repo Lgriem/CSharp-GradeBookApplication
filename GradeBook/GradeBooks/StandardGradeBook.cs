528bfaa3af0e7dd5a709ab674ba31c28fd9eb6c4
Lgriem <78237777+Lgriem@users.noreply.github.com>
2021-01-31 16:46:31 -0600

    First Commit
    Committing to Git

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
